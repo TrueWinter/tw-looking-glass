694a2aa0f0cf1eb82c1e49d4d44e7715c2a7cf87
NdT3Development <admin@ndt3.ml>
2020-09-08 17:50:55 +0200

    - Fixed client-side domain validation - Added better error handling - Added (partially broken) router reachability testing

--- web/index.js
+++ web/index.js
@@ -3,6 +3,8 @@ var ejs = require('ejs');
 var isValidIP = require('is-my-ip-valid')();
 var isValidDomain = require('is-valid-domain');
 var isInSubnet = require('is-in-subnet');
+var isPortReachable = require('is-port-reachable');
+var url = require('url');
 var path = require('path');
 var axios = require('axios');
 var qs = require('qs');
@@ -67,11 +69,50 @@ app.post('/process', function(req, res) {
 			return res.status(400).json({ success: false, message: 'Invalid domain or IP' });
 		}
 
-		axios.post(router.api, qs.stringify({ command: req.body.command, target: req.body.target, key: router.key })).then(function (response) {
+		var routerHost = url.parse(router.api).hostname;
+		var routerPort = url.parse(router.api).port;
+
+		this.CancelToken = axios.CancelToken;
+		this.cancel;
+
+		axios.post(router.api, qs.stringify({
+			command: req.body.command,
+			target: req.body.target,
+			key: router.key
+		}, {
+			cancelToken: new CancelToken(function executor(c) {
+			// An executor function receives a cancel function as a parameter
+			cancel = c;
+			})
+		})).then(function (response) {
 			res.json(response.data);
 		}).catch(function (error) {
 			console.log(error);
-			res.status(500).json(error.message);
+			if (error.response) {
+				// The request was made and the server responded with a status code
+				// that falls out of the range of 2xx
+				console.log(error.response.data);
+				console.log(error.response.status);
+				res.status(error.response.status).json({ success: false, message: error.response.data.message });
+			  } else if (error.request) {
+				// The request was made but no response was received
+				// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+				// http.ClientRequest in node.js
+				console.log(error.request);
+				res.status(500).json({ success: false, message: 'Error while querying router' });
+			  } else {
+				// Something happened in setting up the request that triggered an Error
+				console.log('Error', error.message);
+				res.status(500).json({ success: false, message: error.message });
+			  }
+		});
+
+		isPortReachable(routerPort, { host: routerHost }).then(function (reachable) {
+			if (!reachable) {
+				console.log(router.id + ' appears to be down');
+				cancel();
+				res.status(500).json({ success: false, message: 'Selected router appears to be down' });
+			}
 		});
 	});
 });
